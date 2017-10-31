5aec8674d573d2e6dd5c3ebfeee710a380204ed3
mac <mac@macs-Mac-mini.local>
2017-10-31 16:15:37 +0800

    h

--- tiny.js
+++ tiny.js
@@ -41,7 +41,7 @@ app.use('/wxCallback', middleware(initConfig).getNotify().done((message, req, re
 						  total_fee        : 1,
 						  spbill_create_ip : "10.114.110.1",
 						  trade_type       : 'NATIVE',
-						  notify_url       : 'http://tiny.fndroid.com/wxNotify',
+						  notify_url       : 'tiny.fndroid.com/wxNotify',
 						  product_id       : message.product_id
 	};
 	console.log("1111111");
